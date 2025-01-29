a6cb68ee7960d7f9a9c5aa5ee6327c7da139ae4f
rlawngP91 <annie2104@naver.com>
2025-01-29 17:29:07 +0900

    feat : restore Router.jsx for testing deploy

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -2,6 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 import Main from '../pages/Main/Main';
 import Test from '../pages/Test';
+import MakeReadwith from '../pages/MakeReadwith/MakeReadwith';
+import Info from '../pages/Info/Info';
+import Login from '../pages/login/Login';
+import Onboarding from '../pages/onboarding/Onboarding';
+import Signup from '../pages/signup/Signup';
+import Category from '../pages/category/Category';
+import Profile from '../pages/profile/Profile';
+import Home from '../pages/home/Home';
+import Search from '../pages/search/search';
+import Bookmark from '../pages/bookmark/Bookmark';
+import Record from '../pages/record/Record';
+import ReadingLog from '../pages/readingLog/ReadingLog';
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -12,6 +24,54 @@ const Router = () => {
     {
       path: '/',
       element: <Main />,
+    },
+    {
+      path: '/makeReadwith',
+      element: <MakeReadwith />,
+    },
+    {
+      path: '/info',
+      element: <Info />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/onboarding',
+      element: <Onboarding />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
+    },
+    {
+      path: '/category',
+      element: <Category />,
+    },
+    {
+      path: '/profile',
+      element: <Profile />,
+    },
+    {
+      path: '/home',
+      element: <Home />,
+    },
+    {
+      path: '/search',
+      element: <Search />,
+    },
+    {
+      path: '/bookmark',
+      element: <Bookmark />,
+    },
+    {
+      path: '/record',
+      element: <Record />,
+    },
+    {
+      path: '/readinglog',
+      element: <ReadingLog nickname="TestUser" />,
     },
   ]);
 
