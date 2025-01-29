eb9ff1004051c886c4fe0802b540fab37dda8a31
rlawngP91 <annie2104@naver.com>
2025-01-29 17:38:26 +0900

    fix : removing Search.jsx for test deploy

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -10,7 +10,7 @@ import Signup from '../pages/signup/Signup';
 import Category from '../pages/category/Category';
 import Profile from '../pages/profile/Profile';
 import Home from '../pages/home/Home';
-import Search from '../pages/search/Search';
+
 import Bookmark from '../pages/bookmark/Bookmark';
 import Record from '../pages/record/Record';
 import ReadingLog from '../pages/readingLog/ReadingLog';
@@ -56,10 +56,6 @@ const Router = () => {
     {
       path: '/home',
       element: <Home />,
-    },
-    {
-      path: '/search',
-      element: <Search />,
     },
     {
       path: '/bookmark',
