bef33f49eb95204cd6100ef344e1d11cc7b887ee
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-01-31 02:47:19 +0900

    Feat : 같이읽기 및 혼자읽기 페이지 공통 컴포넌트 구현 중...

--- src/components/RoomHeader/RoomHeader.jsx
+++ src/components/RoomHeader/RoomHeader.jsx
@@ -1,54 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Wrapper,
+  HeaderWrapper,
   Header,
   Title,
   Duration,
-  PercentBar,
-  Type,
-  Button,
+  Tab,
+  Category,
+  Body,
+  Filter,
+  Review,
+  UserList,
 } from './RoomHeader.styles';
 import xbox from '../../assets/xbox.svg';
 import pen from '../../assets/pen.svg';
-import alarm from '../../assets/alarm.svg';
-import refresh from '../../assets/refresh.svg';
+import exit from '../../assets/exit.svg';
 import lock from '../../assets/lock.svg';
-import clock2 from '../../assets/clock2.svg';
-import note from '../../assets/note.svg';
-import rightarrow from '../../assets/rightarrow.svg';
+import grayclock from '../../assets/grayclock.svg';
+import graynote from '../../assets/graynote.svg';
+import grayarrowright from '../../assets/grayarrowright.svg';
+import hamburgermenu from '../../assets/hamburgermenu.svg';
+import userimage from '../../assets/userimage.svg';
+import reply from '../../assets/reply.svg';
+import good from '../../assets/good.svg';
+import grayarrowdown from '../../assets/grayarrowdown.svg';
 import MemberHeader from '../Member/MemberHeader';
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 
 export default function RoomHeader() {
+  const [activeTab, setActiveTab] = useState('페이지별'); // 현재 탭 상태 관리
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab); // 클릭한 탭으로 상태 변경
+  };
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  // 화살표 버튼 클릭 시 상태 토글
+  const toggleUserList = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // 햄버거 메뉴 열림 상태 관리
+
   return (
     <Wrapper>
-      <Header>
-        <img src={xbox} />
-        <div>
-          <img src={pen} />
-          <img src={alarm} />
-          <img src={refresh} />
-        </div>
-      </Header>
-      <Title>
-        <div>책제목</div>
-        <img src={lock} />
-      </Title>
-      <Duration>
-        <img src={clock2} />
-        <div>D-10</div>
-        <img src={note} />
-        <div>75%</div>
-        <img src={rightarrow} />
-      </Duration>
-      <MemberHeader />
-      <PercentBar>
-        <div className="totalpercent"></div>
-        <div className="nowpercent"></div>
-      </PercentBar>
-      <Type>
-        <Button>페이지별 기록</Button>
-        <Button>전체기록</Button>
-      </Type>
+      <HeaderWrapper>
+        <Header>
+          <img src={xbox} />
+          <div>
+            <img src={pen} />
+            <img src={exit} />
+          </div>
+        </Header>
+        <Title>
+          <div>책제목</div>
+          <img src={lock} />
+        </Title>
+        <div className="roomname">어쩌구 저쩌구 독서방</div>
+        <Duration>
+          <img src={grayclock} />
+          <div>D-10</div>
+          <img src={graynote} />
+          <div>75%</div>
+          <img
+            src={isExpanded ? grayarrowdown : grayarrowright}
+            onClick={toggleUserList}
+            style={{ cursor: 'pointer' }}
+          />
+        </Duration>
+        {/* 참가 유저 목록 (isExpanded가 true일 때만 표시) */}
+        {isExpanded && (
+          <UserList>
+            <MemberHeader />
+          </UserList>
+        )}
+        <Tab>
+          <Category
+            $isActive={activeTab === '페이지별'}
+            onClick={() => handleTabClick('페이지별')}
+          >
+            페이지별 기록
+          </Category>
+          <Category
+            $isActive={activeTab === '전체'}
+            onClick={() => handleTabClick('전체')}
+          >
+            전체 기록
+          </Category>
+        </Tab>
+      </HeaderWrapper>
+
+      {activeTab === '페이지별' ? (
+        <>
+          <Filter>
+            <div className="pagerange">페이지 범위</div>
+            <div className="pagearray">페이지 순</div>
+          </Filter>
+          <Review>
+            <div className="head">
+              <div className="main">
+                <img src={userimage} />
+                <div className="name">닉네임</div>
+                <div className="time">2024.11.14 23:04:28</div>
+              </div>
+              <img src={hamburgermenu} onClick={() => setIsMenuOpen(true)} />
+              {isMenuOpen && (
+                <HamburgerMenu onClose={() => setIsMenuOpen(false)} />
+              )}
+            </div>
+            <div className="body">
+              <div className="page">100p</div>
+              <div className="content">
+                해리는 모자가 아닌 본인 스스로 그리핀도르로 선택했다. 진정한
+                용기는 능력이 아니라 선택의 결과로 나타난다.
+              </div>
+            </div>
+            <div className="bottom">
+              <img src={reply} />
+              <div>3</div>
+              <img src={good} />
+              <div>55</div>
+            </div>
+          </Review>
+        </>
+      ) : (
+        <>
+          <Review></Review>
+        </>
+      )}
+      <Body></Body>
     </Wrapper>
   );
 }
