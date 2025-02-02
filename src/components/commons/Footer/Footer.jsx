1260ddb448217d4d94ab20c7ff231ce6bd5de7b3
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-02-02 13:58:55 +0900

    fix : Footer 네비게이터, 조건부렌더링 처리

--- src/components/commons/Footer/Footer.jsx
+++ src/components/commons/Footer/Footer.jsx
@@ -4,24 +4,47 @@ import home from '../../../assets/home.svg';
 import bookwrite from '../../../assets/bookwrite.svg';
 import search from '../../../assets/search.svg';
 import my from '../../../assets/my.svg';
+import homeActive from '../../../assets/homeActive.svg';
+import bookwriteActive from '../../../assets/bookwriteActive.svg';
+import searchActive from '../../../assets/searchActive.svg';
+import myActive from '../../../assets/myActive.svg';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Footer() {
+  const location = useLocation(); // 현재 경로 가져오기
+  const navigate = useNavigate(); // 페이지 이동을 위한 네비게이션 함수
+
+  // 현재 페이지와 버튼이 매칭되면 파란색 적용
+  const isActive = (path) => location.pathname === path;
+
   return (
     <Wrapper>
-      <Button>
-        <img src={home} alt="홈" />
+      <Button onClick={() => navigate('/home')} $isActive={isActive('/home')}>
+        <img src={isActive('/home') ? homeActive : home} alt="홈" />
         <div>홈</div>
       </Button>
-      <Button>
-        <img src={bookwrite} alt="도서기록장" />
+      <Button
+        onClick={() => navigate('/bookmark')}
+        $isActive={isActive('/bookmark')}
+      >
+        <img
+          src={isActive('/bookmark') ? bookwriteActive : bookwrite}
+          alt="도서기록장"
+        />{' '}
         <div>도서기록장</div>
       </Button>
-      <Button>
-        <img src={search} alt="책찾기" />
+      <Button
+        onClick={() => navigate('/search')}
+        $isActive={isActive('/search')}
+      >
+        <img src={isActive('/search') ? searchActive : search} alt="책찾기" />
         <div>책찾기</div>
       </Button>
-      <Button>
-        <img src={my} alt="마이페이지" />
+      <Button
+        onClick={() => navigate('/mypage')}
+        $isActive={isActive('/mypage')}
+      >
+        <img src={isActive('/mypage') ? myActive : my} alt="마이페이지" />
         <div>마이페이지</div>
       </Button>
     </Wrapper>
