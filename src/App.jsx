f6c17bee9cce8cd5ba138b36c3cc2db9f5f08326
rlawngP91 <annie2104@naver.com>
2025-02-14 07:29:18 +0000

    Fix : 폰트 설정 완료

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,43 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: "Pretendard";
-    src: url("./Pretendard/Pretendard-Medium.otf") format("opentype");
+    font-weight: 100;
+    src: url("/Pretendard/Pretendard-Thin.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 200;
+    src: url("/Pretendard/Pretendard-ExtraLight.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 300;
+    src: url("/Pretendard/Pretendard-Light.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 400;
+    src: url("/Pretendard/Pretendard-Regular.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 500;
+    src: url("/Pretendard/Pretendard-Medium.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 600;
+    src: url("/Pretendard/Pretendard-SemiBold.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 700;
+    src: url("/Pretendard/Pretendard-Bold.otf") format("opentype");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 800;
+    src: url("/Pretendard/Pretendard-ExtraBold.otf") format("opentype");
   }
   body {
     font-family: "Pretendard";
