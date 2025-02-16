ef53b54fe8c7bdb946a81de3a454662467ba1676
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-02-16 16:29:31 +0900

    Fix : RW헤더 navigate 추가

--- src/components/RWHeader/RWHeader.jsx
+++ src/components/RWHeader/RWHeader.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Wrapper, HeaderWrapper } from './RWHeader.styles';
 import leftarrow from '../../assets/leftarrow.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function RWHeader() {
+  const navigate = useNavigate();
   return (
     <>
       <Wrapper>
         <HeaderWrapper>
-          <img src={leftarrow} alt="뒤로가기" />
+          <img
+            src={leftarrow}
+            alt="뒤로가기"
+            onClick={() => navigate(-1)}
+            style={{ cursor: 'pointer' }}
+          />
           <div>방 생성하기</div>
         </HeaderWrapper>
       </Wrapper>
