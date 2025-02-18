ec3411c2afb4dabd794626261e07fa4edc1728d4
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-02-19 07:08:37 +0900

    Fix : 비밀번호 삭제 기능 추가 및 로직 수정

--- src/components/InfoBody/ButtonGroup2.jsx
+++ src/components/InfoBody/ButtonGroup2.jsx
@@ -24,6 +24,28 @@ export const Button = styled.button`
   /* 기본 스타일 제거 */
   border: none; /* 테두리 제거 */
   outline: none; /* 포커스 시 나오는 아웃라인 제거 */
+  cursor: pointer;
+
+  color: #fff;
+  font-family: Pretandard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px; /* 106.667% */
+  letter-spacing: 0.5px;
+`;
+
+export const Button1 = styled.button`
+  width: 167px;
+  height: 42px;
+  border-radius: 9px;
+  border: 1px solid #cecbcb;
+  background: ${(props) => (props.$public ? '#6aa5f8' : '#A9A9A9')};
+
+  /* 기본 스타일 제거 */
+  border: none; /* 테두리 제거 */
+  outline: none; /* 포커스 시 나오는 아웃라인 제거 */
+  cursor: ${(props) => (props.$public ? 'pointer' : 'not-allowed')};
 
   color: #fff;
   font-family: Pretandard;
@@ -40,24 +62,34 @@ export default function ButtonGroup2({ roomData, roomId }) {
   const [popupVisible, setPopupVisible] = useState(false);
 
   const handleRecordClick = () => {
-    if (roomData.member) {
-      // ✅ 멤버라면 네비게이트
+    if (roomData?.member) {
+      // ✅ 멤버이면 바로 네비게이트 실행
       if (roomId) {
         navigate(`/rooms/${roomId}/info`);
       } else {
         console.error('roomId가 라우터 파라미터로 전달되지 않았습니다.');
       }
     } else {
-      // ✅ 멤버가 아니라면 팝업 띄우기
-      setPopupVisible(true);
+      // ✅ 멤버가 아니고, 방이 비공개(`public === false`)라면 비밀번호 팝업 띄우기
+      if (roomData?.public === false) {
+        setPopupVisible(true);
+        return;
+      }
+
+      // ✅ 멤버가 아니지만 방이 공개(`public === true`)라면 네비게이트 실행
+      navigate(`/rooms/${roomId}/info`);
     }
   };
 
   return (
     <ButtonGroupWrapper>
-      <Button onClick={() => navigate(`/rooms/${roomId}/preview`)}>
+      <Button1
+        onClick={() => navigate(`/rooms/${roomId}/preview`)}
+        disabled={!roomData?.public} // roomData가 없거나 public이 false면 비활성화
+        $public={roomData?.public} // Styled Components에서 사용할 prop
+      >
         미리보기
-      </Button>
+      </Button1>
       <Button onClick={handleRecordClick}>기록하기</Button>
       {popupVisible && (
         <RoomPasswordPopup
