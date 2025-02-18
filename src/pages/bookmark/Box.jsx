699edb5f85e7bff24505bd3ed5d4c0fb4c000840
KingSeokJin <sjin7629@gmail.com>
2025-02-19 00:59:26 +0900

    [fix] 즐겨찾기 클릭시 isbn페이지로

--- src/pages/bookmark/Box.jsx
+++ src/pages/bookmark/Box.jsx
@@ -11,13 +11,27 @@ const Box = ({
   isDeleteMode,
   isChecked,
   onToggle,
+  onClick,
 }) => {
-  const handleClick = () => {
-    onToggle(bookTitle, !isChecked); // 선택 상태를 상위 컴포넌트로 전달
+  const handleBoxClick = () => {
+    if (isDeleteMode) {
+      // 만약 삭제 모드이면, 체크 아이콘을 클릭했을 때만 onToggle이 실행되도록 처리
+      // (이미 check-icon에 onClick이 있으므로 여기서는 아무것도 하지 않음)
+      return;
+    } else {
+      // 삭제 모드가 아니라면, Box 전체 클릭 시 onClick 호출
+      onClick && onClick();
+    }
+  };
+
+  // 기존 체크 아이콘 클릭 핸들러 (삭제 모드일 때)
+  const handleCheckClick = (e) => {
+    e.stopPropagation(); // 부모 클릭 이벤트 방지
+    onToggle(bookTitle, !isChecked);
   };
 
   return (
-    <Container>
+    <Container onClick={handleBoxClick}>
       <img className="cover" src={imgSrc} alt="책 표지" />
       <div className="right">
         <div className="info-container">
@@ -30,7 +44,7 @@ const Box = ({
           src={isChecked ? Checked : Unchecked}
           alt={isChecked ? 'Checked' : 'Unchecked'}
           className="check-icon"
-          onClick={handleClick}
+          onClick={handleCheckClick}
         />
       )}
     </Container>
@@ -45,6 +59,7 @@ Box.propTypes = {
   isDeleteMode: PropTypes.bool.isRequired,
   isChecked: PropTypes.bool.isRequired, // 상위에서 isChecked 상태를 받아옴
   onToggle: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Box;
