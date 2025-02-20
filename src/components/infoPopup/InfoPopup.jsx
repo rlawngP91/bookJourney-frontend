368e985bf8e0629624ede3e49e47a78c77d56472
KingSeokJin <sjin7629@gmail.com>
2025-02-20 11:49:22 +0900

    [fix] 액세스토큰 재발급 수정

--- src/components/infoPopup/InfoPopup.jsx
+++ src/components/infoPopup/InfoPopup.jsx
@@ -10,13 +10,13 @@ const InfoPopup = ({ onLine1Click, onLine2Click, className }) => {
         {/* 
         <img className="left-img" src={InfoCircle} alt="인포아이콘" />
         */}
-        <div className="info2">책 및 방정보</div>
+        <div className="info2">책 및 방 정보</div>
       </div>
       <div className="second-line" onClick={onLine2Click}>
         {/* 
         <img className="left-img" src={Trash} alt="쓰레기통" />
         */}
-        <div className="delete-record2">진행중인 기록에서 삭제</div>
+        <div className="delete-record2">진행중 기록에서 삭제</div>
       </div>
     </Container>
   );
